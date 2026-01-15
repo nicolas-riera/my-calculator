cbed7d76476db99afce5db69e7bcaa3a57f0ea2d
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-15 10:02:39 +0100

    Simplified float/int selection for result

--- src/calculator.py
+++ src/calculator.py
@@ -150,14 +150,19 @@ def calc_result(calc_input, result):
         calc_input, result = calc_choice(result)
 
         if result != None:
+                
             if (float(result) % 1) == 0.0:
-                print(f"{calc_input} is equal to {int(float(result))}")
-            else:
-                print(f"{calc_input} is equal to {round(float(result), 10)}")
+                result = int(float(result))
+            else :
+                result = float(result)
+            
+            print(f"{calc_input} is equal to {round(result, 10)}")
             time.sleep(0.2)
             input("Press Enter to continue..")
             clear()
+
         write_history(calc_input, result)
+
         return result
     else:
 
@@ -166,6 +171,12 @@ def calc_result(calc_input, result):
         calc_input, new_result = calc_choice(result)
 
         if new_result != None:
+
+            if (float(new_result) % 1) == 0.0:
+                new_result = int(float(new_result))
+            else :
+                new_result = float(new_result)
+
             if (float(new_result) % 1) == 0.0:
                 print(f"{previous_result} {calc_input} is equal to {int(float(new_result))}")
             else:
