2b9db15b7b84ca7c32731f8d3aa58c4023a81660
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-16 13:13:24 +0100

    Various changes for pyinstaller

--- src/history.py
+++ src/history.py
@@ -74,7 +74,7 @@ def display_history():
             print(f"{Fore.cyan}{e["input"]} {Fore.white}= {Fore.blue}{e["result"]}\n")
 
         try:
-            usr_input = int(input(f"{Fore.red}1. Clear History\n{Fore.black}0. Exit\n{Fore.white}Enter your choice...\n"))
+            usr_input = int(input(f"{Fore.red}1. Clear History\n{Fore.RGB(150, 150, 150)}0. Exit\n{Fore.white}Enter your choice...\n"))
      
         except ValueError:
             clear()
--- src/menu.py
+++ src/menu.py
@@ -27,9 +27,9 @@ def menu():
             try:
                 # Different menus depending on the state of the calculator
                 if result == None:
-                    usr_input = int(input(f"{Fore.blue}1. Calculate\n{Fore.yellow}2. History\n{Fore.black}0. Exit\n{Fore.white}Enter your choice...\n"))
+                    usr_input = int(input(f"{Fore.blue}1. Calculate\n{Fore.yellow}2. History\n{Fore.RGB(150, 150, 150)}0. Exit\n{Fore.white}Enter your choice...\n"))
                 else:
-                    usr_input = int(input(f"{Fore.blue}1. Calculate\n{Fore.yellow}2. History\n{Fore.red}3. Erase result\n{Fore.black}0. Exit\n{Fore.white}Enter your choice...\n"))
+                    usr_input = int(input(f"{Fore.blue}1. Calculate\n{Fore.yellow}2. History\n{Fore.red}3. Erase result\n{Fore.RGB(150, 150, 150)}0. Exit\n{Fore.white}Enter your choice...\n"))
                 clear()
 
             except ValueError:
@@ -55,7 +55,12 @@ def menu():
 
                 case 0:
                     clear()
-                    exit()
+                    
+                    # pyinstaller for .exe doesn't have exit()
+                    try:
+                        exit()
+                    except:
+                        break
 
                 case _:
                     error()
